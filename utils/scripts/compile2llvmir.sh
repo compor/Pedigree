f598f420ef645886ce643e9a7817dffc26258d08
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-02-16 15:44:19 +0000

    update script

--- utils/scripts/compile2llvmir.sh
+++ utils/scripts/compile2llvmir.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+declare -a TMP_FILES
+
+function cleanup_tmp() {
+  echo "Cleaning up temp files..."
+
+  for tmpfile in ${TMP_FILES[@]}; do
+    rm -f ${tmpfile}
+  done
+}
+
+trap cleanup_tmp EXIT
+
 for CUR_FILE in "${@}"; do 
   FILE_PREFIX=${CUR_FILE%.*}
   PREFIX_LEN=${#FILE_PREFIX}
@@ -7,9 +19,15 @@ for CUR_FILE in "${@}"; do
   PREFIX_OFFSET=$(echo "${PREFIX_LEN}-1" | bc)
 
   BITCODE_FILENAME="${CUR_FILE:0:${PREFIX_LEN}}.ll"
+  TMP_FILE="tmp.${BITCODE_FILENAME}"
 
-  clang -c -S -emit-llvm ${CUR_FILE} -o ${BITCODE_FILENAME}
-  #clang -g -c -S -emit-llvm ${CUR_FILE} -o ${BITCODE_FILENAME}
+  TMP_FILES+=( ${TMP_FILE} )
 
-  [[ $? -eq 0 ]] && echo "Compiled ${CUR_FILE} to ${BITCODE_FILENAME}"
+  clang -c -S -emit-llvm ${CUR_FILE} -o ${TMP_FILE}
+  [[ ${?} -ne 0 ]] && exit 1
+  echo "Compiled ${CUR_FILE} to ${TMP_FILE}"
+
+  opt -S -mem2reg ${TMP_FILE} -o ${BITCODE_FILENAME}
+  [[ ${?} -ne 0 ]] && exit 1
+  echo "Compiled ${BITCODE_FILENAME} to ${TMP_FILE}"
 done
