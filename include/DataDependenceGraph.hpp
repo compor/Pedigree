765a050111c4022208f4b08c71e802e568b0ce86
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-02-26 13:34:13 +0000

    add llvm graph traits

--- include/DataDependenceGraph.hpp
+++ include/DataDependenceGraph.hpp
@@ -11,6 +11,9 @@
 #include "llvm/IR/InstVisitor.h"
 // using llvm::InstVisitor
 
+#include "llvm/ADT/STLExtras.h"
+// using llvm::mapped_iterator
+
 #include <vector>
 // using std::vector
 
@@ -119,6 +122,11 @@ public:
   inline decltype(auto) begin() const { return m_NodeMap.begin(); }
   inline decltype(auto) end() const { return m_NodeMap.end(); }
 
+  const DependenceGraphNode *getEntryNode() const { return begin()->second; }
+  DependenceGraphNode *getEntryNode() {
+    return const_cast<DependenceGraphNode *>(getEntryNode());
+  }
+
 private:
   NodeMapTy m_NodeMap;
 };
@@ -145,5 +153,32 @@ public:
 };
 
 } // namespace pedigree end
+
+namespace llvm {
+
+using namespace pedigree;
+
+template <> struct GraphTraits<DataDependenceGraph> {
+  using GraphTy = DataDependenceGraph;
+  using NodeType = DependenceGraphNode;
+
+  using GraphPairTy = std::pair<llvm::Instruction *, DependenceGraphNode *>;
+  using DerefFuncTy = std::function<DependenceGraphNode &(GraphPairTy)>;
+
+  using nodes_iterator = llvm::mapped_iterator<GraphTy::iterator, DerefFuncTy>;
+
+  static NodeType *getEntryNode(GraphTy *G) { return G->getEntryNode(); }
+
+  static nodes_iterator nodes_begin(GraphTy *G) {
+    return llvm::map_iterator(G->begin(), DerefFuncTy(GraphDeref));
+  }
+  static nodes_iterator nodes_end(GraphTy *G) {
+    return llvm::map_iterator(G->end(), DerefFuncTy(GraphDeref));
+  }
+
+  static DependenceGraphNode &GraphDeref(GraphPairTy P) { return *P.second; }
+};
+
+} // namespace llvm end
 
 #endif // header
