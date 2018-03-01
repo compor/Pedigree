4d8adaaaad7dd3e76a1586ca5a88b0afd6d80dc2
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-03-01 11:04:32 +0000

    move dependence info to separate header

--- include/DataDependenceGraph.hpp
+++ include/DataDependenceGraph.hpp
@@ -5,6 +5,8 @@
 #ifndef DATADEPENDENCEGRAPH_HPP
 #define DATADEPENDENCEGRAPH_HPP
 
+#include "Dependence.hpp"
+
 #include "llvm/IR/Instruction.h"
 // using llvm::Instruction
 
@@ -30,25 +32,6 @@
 // using assert
 
 namespace pedigree {
-
-enum class DependenceType : uint8_t {
-  unknown = 0,
-  flow = 1,
-  anti = 2,
-  out = 4,
-};
-
-enum class DependenceOrigin : uint8_t {
-  unknown = 0,
-  data = 1,
-  memory = 2,
-  control = 4,
-};
-
-struct DataDependenceInfo {
-  DependenceType type;
-  DependenceOrigin origin;
-};
 
 class DependenceGraphNode {
 public:
---
+++ include/Dependence.hpp
@@ -0,0 +1,31 @@
+//
+//
+//
+
+#ifndef DEPENDENCE_HPP
+#define DEPENDENCE_HPP
+
+namespace pedigree {
+
+enum class DependenceType : uint8_t {
+  unknown = 0,
+  flow = 1,
+  anti = 2,
+  out = 4,
+};
+
+enum class DependenceOrigin : uint8_t {
+  unknown = 0,
+  data = 1,
+  memory = 2,
+  control = 4,
+};
+
+struct DataDependenceInfo {
+  DependenceType type;
+  DependenceOrigin origin;
+};
+
+} // namespace pedigree end
+
+#endif // header
