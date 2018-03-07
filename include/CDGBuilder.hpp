b2a58c4d3eab65806b0f1370fa032dfa3e631516
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-03-07 20:44:07 +0000

    add cdg builder

--- include/CDGBuilder.hpp
+++ include/CDGBuilder.hpp
@@ -14,6 +14,12 @@
 #include "llvm/IR/BasicBlock.h"
 // using llvm::BasicBlock
 
+#include "llvm/IR/Function.h"
+// using llvm::Function
+
+#include "llvm/Analysis/PostDominators.h"
+// using llvm::PostDominatorTree
+
 namespace pedigree {
 
 class CDGBuilder {
@@ -21,6 +27,22 @@ class CDGBuilder {
 
 public:
   CDGBuilder(CDG &Graph) : m_Graph(Graph) {}
+
+  void build(const llvm::Function &CurFunc) {
+    llvm::PostDominatorTree curPDT;
+    curPDT.DT->recalculate(const_cast<llvm::Function &>(CurFunc));
+
+    PostDominanceFrontierBase<llvm::BasicBlock> pdf;
+    pdf.analyze(*curPDT.DT);
+
+    for (auto &f : pdf) {
+      auto dst = m_Graph.getOrInsertNode(f.first);
+      for (auto &e : f.second) {
+        auto src = m_Graph.getOrInsertNode(e);
+        src->addDependentNode(dst);
+      }
+    }
+  }
 };
 
 } // namespace pedigree end
