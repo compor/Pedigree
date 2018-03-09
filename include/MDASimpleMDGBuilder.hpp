6f0367b2768c373a24ea74e9c47858058990473d
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-03-09 19:37:21 +0000

    add utility method for readability

--- include/MDASimpleMDGBuilder.hpp
+++ include/MDASimpleMDGBuilder.hpp
@@ -31,28 +31,16 @@
 
 namespace pedigree {
 
+// TODO maybe we should consider providing an option for not including nodes
+// in the graph unless they have an edge
+
 class MDALocalMDGBuilder : public llvm::InstVisitor<MDALocalMDGBuilder> {
   MDG &m_Graph;
   llvm::MemoryDependenceAnalysis &m_MDA;
   bool m_isBlockLocal;
 
-public:
-  MDALocalMDGBuilder(MDG &Graph, const llvm::MemoryDependenceAnalysis &MDA,
-                     bool isBlockLocal = true)
-      : m_Graph(Graph),
-        m_MDA(const_cast<llvm::MemoryDependenceAnalysis &>(MDA)),
-        m_isBlockLocal(isBlockLocal) {}
-
-  template <typename T> void build(T &Unit) { visit(Unit); }
-
-  void visitInstruction(llvm::Instruction &CurInstruction) {
-    // TODO maybe we should consider providing an option for not including nodes
-    // in the graph unless they have an edge
+  void visitMemRefInstruction(llvm::Instruction &CurInstruction) {
     auto dst = m_Graph.getOrInsertNode(&CurInstruction);
-
-    if (!CurInstruction.mayReadOrWriteMemory() &&
-        !llvm::ImmutableCallSite(&CurInstruction))
-      return;
 
     auto query = m_MDA.getDependency(&CurInstruction);
     assert(query.getInst() && "Dependee instruction is null!");
@@ -74,6 +62,23 @@ public:
       auto src = m_Graph.getOrInsertNode(e);
       src->addDependentNode(dst);
     }
+  }
+
+public:
+  MDALocalMDGBuilder(MDG &Graph, const llvm::MemoryDependenceAnalysis &MDA,
+                     bool isBlockLocal = true)
+      : m_Graph(Graph),
+        m_MDA(const_cast<llvm::MemoryDependenceAnalysis &>(MDA)),
+        m_isBlockLocal(isBlockLocal) {}
+
+  template <typename T> void build(T &Unit) { visit(Unit); }
+
+  void visitInstruction(llvm::Instruction &CurInstruction) {
+    if (!CurInstruction.mayReadOrWriteMemory() &&
+        !llvm::ImmutableCallSite(&CurInstruction))
+      return;
+
+    visitMemRefInstruction(CurInstruction);
   }
 };
 
