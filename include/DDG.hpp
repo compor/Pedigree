32581778dd2e942c06a0739bd1bd4a1db9f929dd
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-03-19 10:50:22 +0000

    use generic graph traits helpers in mdg and ddg

--- include/DDG.hpp
+++ include/DDG.hpp
@@ -41,33 +41,7 @@ struct GraphTraits<pedigree::DataDependenceNode *>
 
 template <>
 struct GraphTraits<pedigree::DDG *>
-    : public GraphTraits<pedigree::DataDependenceNode *> {
-  using GraphTy = pedigree::DDG;
-
-  using NodePairTy = GraphTy::value_type;
-
-  static NodeType &NodeDeref(NodePairTy &P) { return *P.second.get(); }
-  using NodeDerefFuncTy = std::function<decltype(NodeDeref)>;
-
-  using nodes_iterator =
-      llvm::mapped_iterator<GraphTy::iterator, NodeDerefFuncTy>;
-
-  static NodeType *getEntryNode(GraphTy *G) { return G->getEntryNode(); }
-
-  static nodes_iterator nodes_begin(GraphTy *G) {
-    using std::begin;
-    return llvm::map_iterator(begin(*G), NodeDerefFuncTy(NodeDeref));
-  }
-
-  static nodes_iterator nodes_end(GraphTy *G) {
-    using std::end;
-    return llvm::map_iterator(end(*G), NodeDerefFuncTy(NodeDeref));
-  }
-
-  static unsigned size(GraphTy *G) {
-    return static_cast<unsigned>(G->numVertices());
-  }
-};
+    : public pedigree::LLVMDependenceGraphTraitsBase<pedigree::DDG *> {};
 
 } // namespace llvm end
 
--- include/MDG.hpp
+++ include/MDG.hpp
@@ -41,33 +41,7 @@ struct GraphTraits<pedigree::MemoryDependenceNode *>
 
 template <>
 struct GraphTraits<pedigree::MDG *>
-    : public GraphTraits<pedigree::MemoryDependenceNode *> {
-  using GraphTy = pedigree::MDG;
-
-  using NodePairTy = GraphTy::value_type;
-
-  static NodeType &NodeDeref(NodePairTy &P) { return *P.second.get(); }
-  using NodeDerefFuncTy = std::function<decltype(NodeDeref)>;
-
-  using nodes_iterator =
-      llvm::mapped_iterator<GraphTy::iterator, NodeDerefFuncTy>;
-
-  static NodeType *getEntryNode(GraphTy *G) { return G->getEntryNode(); }
-
-  static nodes_iterator nodes_begin(GraphTy *G) {
-    using std::begin;
-    return llvm::map_iterator(begin(*G), NodeDerefFuncTy(NodeDeref));
-  }
-
-  static nodes_iterator nodes_end(GraphTy *G) {
-    using std::end;
-    return llvm::map_iterator(end(*G), NodeDerefFuncTy(NodeDeref));
-  }
-
-  static unsigned size(GraphTy *G) {
-    return static_cast<unsigned>(G->numVertices());
-  }
-};
+    : public pedigree::LLVMDependenceGraphTraitsBase<pedigree::MDG *> {};
 
 } // namespace llvm end
 
