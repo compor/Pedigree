acd116e7bfd58ac047814ee7010b23de596a6d8e
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-03-21 17:26:32 +0000

    move traits to separate file

--- include/Converter.hpp
+++ include/Converter.hpp
@@ -7,55 +7,7 @@
 
 #include "Config.hpp"
 
-namespace llvm {
-class Instruction;
-class BasicBlock;
-class Function;
-} // namespace llvm end
-
-namespace pedigree {
-
-struct instruction_unit_tag {};
-struct basicblock_unit_tag {};
-struct function_unit_tag {};
-
-template <typename UnitT> struct unit_traits {
-  using unit_category = typename UnitT::unit_category;
-};
-
-template <> struct unit_traits<llvm::Instruction> {
-  using unit_category = instruction_unit_tag;
-};
-
-template <> struct unit_traits<llvm::Instruction *> {
-  using unit_category = instruction_unit_tag;
-};
-
-template <> struct unit_traits<llvm::BasicBlock> {
-  using unit_category = basicblock_unit_tag;
-};
-
-template <> struct unit_traits<llvm::BasicBlock *> {
-  using unit_category = basicblock_unit_tag;
-};
-
-template <> struct unit_traits<llvm::Function> {
-  using unit_category = function_unit_tag;
-};
-
-template <> struct unit_traits<llvm::Function *> {
-  using unit_category = function_unit_tag;
-};
-
-template <typename FromCategoryT, typename ToCategoryT>
-struct is_unit_adaptable {
-  static constexpr bool value = false;
-};
-
-template <>
-struct is_unit_adaptable<basicblock_unit_tag, instruction_unit_tag> {
-  static constexpr bool value = true;
-};
+#include "traits/Unit.hpp"
 
 template <typename FromNodeT, typename ToNodeT>
 void Convert(const GenericDependenceGraph<FromNodeT> &From,
---
+++ include/traits/Unit.hpp
@@ -0,0 +1,66 @@
+//
+//
+//
+
+#ifndef PEDIGREE_TRAITS_UNIT_HPP
+#define PEDIGREE_TRAITS_UNIT_HPP
+
+#include "Config.hpp"
+
+namespace llvm {
+class Instruction;
+class BasicBlock;
+class Function;
+} // namespace llvm end
+
+namespace pedigree {
+
+struct instruction_unit_tag {};
+struct basicblock_unit_tag {};
+struct function_unit_tag {};
+
+//
+
+template <typename UnitT> struct unit_traits {
+  using unit_category = typename UnitT::unit_category;
+};
+
+template <> struct unit_traits<llvm::Instruction> {
+  using unit_category = instruction_unit_tag;
+};
+
+template <> struct unit_traits<llvm::Instruction *> {
+  using unit_category = instruction_unit_tag;
+};
+
+template <> struct unit_traits<llvm::BasicBlock> {
+  using unit_category = basicblock_unit_tag;
+};
+
+template <> struct unit_traits<llvm::BasicBlock *> {
+  using unit_category = basicblock_unit_tag;
+};
+
+template <> struct unit_traits<llvm::Function> {
+  using unit_category = function_unit_tag;
+};
+
+template <> struct unit_traits<llvm::Function *> {
+  using unit_category = function_unit_tag;
+};
+
+//
+
+template <typename FromCategoryT, typename ToCategoryT>
+struct is_unit_adaptable {
+  static constexpr bool value = false;
+};
+
+template <>
+struct is_unit_adaptable<basicblock_unit_tag, instruction_unit_tag> {
+  static constexpr bool value = true;
+};
+
+} // namespace pedigree end
+
+#endif // header
