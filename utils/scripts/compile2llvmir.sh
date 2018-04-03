38084389a1939946e1d72a6128880b929f5e6120
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-04-03 18:54:44 +0100

    lint script

--- utils/scripts/compile2llvmir.sh
+++ utils/scripts/compile2llvmir.sh
@@ -5,8 +5,8 @@ declare -a TMP_FILES
 function cleanup_tmp() {
   echo "Cleaning up temp files..."
 
-  for tmpfile in ${TMP_FILES[@]}; do
-    rm -f ${tmpfile}
+  for tmpfile in "${TMP_FILES[@]}"; do
+    rm -f "${tmpfile}"
   done
 }
 
@@ -16,18 +16,16 @@ for CUR_FILE in "${@}"; do
   FILE_PREFIX=${CUR_FILE%.*}
   PREFIX_LEN=${#FILE_PREFIX}
   
-  PREFIX_OFFSET=$(echo "${PREFIX_LEN}-1" | bc)
-
   BITCODE_FILENAME="${CUR_FILE:0:${PREFIX_LEN}}.ll"
   TMP_FILE="tmp.${BITCODE_FILENAME}"
 
   TMP_FILES+=( ${TMP_FILE} )
 
-  clang -c -S -emit-llvm ${CUR_FILE} -o ${TMP_FILE}
+  clang -c -S -emit-llvm "${CUR_FILE}" -o "${TMP_FILE}"
   [[ ${?} -ne 0 ]] && exit 1
   echo "Compiled ${CUR_FILE} to ${TMP_FILE}"
 
-  opt -S -mem2reg ${TMP_FILE} -o ${BITCODE_FILENAME}
+  opt -S -mem2reg "${TMP_FILE}" -o "${BITCODE_FILENAME}"
   [[ ${?} -ne 0 ]] && exit 1
   echo "Compiled ${BITCODE_FILENAME} to ${TMP_FILE}"
 done
