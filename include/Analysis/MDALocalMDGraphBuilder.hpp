5c8bfc536b8f287c6ebd4e31594923540ac28fa4
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-04-05 14:18:28 +0100

    factor out query result handling

--- include/Analysis/MDALocalMDGraphBuilder.hpp
+++ include/Analysis/MDALocalMDGraphBuilder.hpp
@@ -61,108 +61,6 @@ private:
   llvm::MemoryDependenceAnalysis &MDA;
   AnalysisScope Scope;
 
-  void getInterproceduralDependees(llvm::CallSite Dst) {
-    if (!Dst)
-      return;
-
-    auto &results = MDA.getNonLocalCallDependency(Dst);
-    auto dst = Dst.getInstruction();
-
-    for (auto &e : results) {
-      BasicDependenceInfo info{DependenceOrigin::Memory,
-                               DependenceHazard::Unknown};
-
-      auto &result = e.getResult();
-      auto src = result.getInst();
-
-      if (result.isDef()) {
-        if (src->mayReadFromMemory() && dst->mayReadFromMemory())
-          assert(true && "A RAW hazard was reported!");
-
-        if (src->mayReadFromMemory() && dst->mayWriteToMemory())
-          info.hazards |= DependenceHazard::Anti;
-
-        if (src->mayWriteToMemory() && dst->mayReadFromMemory())
-          info.hazards |= DependenceHazard::Flow;
-
-        if (src->mayWriteToMemory() && dst->mayWriteToMemory())
-          info.hazards |= DependenceHazard::Out;
-      } else if (result.isClobber()) {
-        if (dst->mayReadFromMemory())
-          info.hazards |= DependenceHazard::Flow;
-
-        if (dst->mayWriteToMemory())
-          info.hazards |= DependenceHazard::Out;
-      }
-
-      addDependenceWithInfo(*src, *dst, info);
-    }
-  }
-
-  void getFunctionLocalDependees(llvm::Instruction &Dst) {
-    llvm::SmallVector<llvm::NonLocalDepResult, 8> result;
-    MDA.getNonLocalPointerDependency(&Dst, result);
-
-    for (const auto &e : result) {
-      BasicDependenceInfo info{DependenceOrigin::Memory,
-                               DependenceHazard::Unknown};
-
-      auto &result = e.getResult();
-      auto src = result.getInst();
-
-      if (result.isDef()) {
-        if (src->mayReadFromMemory() && Dst.mayReadFromMemory())
-          assert(true && "A RAW hazard was reported!");
-
-        if (src->mayReadFromMemory() && Dst.mayWriteToMemory())
-          info.hazards |= DependenceHazard::Anti;
-
-        if (src->mayWriteToMemory() && Dst.mayReadFromMemory())
-          info.hazards |= DependenceHazard::Flow;
-
-        if (src->mayWriteToMemory() && Dst.mayWriteToMemory())
-          info.hazards |= DependenceHazard::Out;
-      } else if (result.isClobber()) {
-        if (Dst.mayReadFromMemory())
-          info.hazards |= DependenceHazard::Flow;
-
-        if (Dst.mayWriteToMemory())
-          info.hazards |= DependenceHazard::Out;
-      }
-
-      addDependenceWithInfo(*src, Dst, info);
-    }
-  }
-
-  void getBlockLocalDependees(llvm::MemDepResult &Query,
-                              llvm::Instruction &Dst) {
-    BasicDependenceInfo info{DependenceOrigin::Memory,
-                             DependenceHazard::Unknown};
-    auto src = Query.getInst();
-
-    if (Query.isDef()) {
-      if (src->mayReadFromMemory() && Dst.mayReadFromMemory())
-        assert(true && "A RAW hazard was reported!");
-
-      if (src->mayReadFromMemory() && Dst.mayWriteToMemory())
-        info.hazards |= DependenceHazard::Anti;
-
-      if (src->mayWriteToMemory() && Dst.mayReadFromMemory())
-        info.hazards |= DependenceHazard::Flow;
-
-      if (src->mayWriteToMemory() && Dst.mayWriteToMemory())
-        info.hazards |= DependenceHazard::Out;
-    } else if (Query.isClobber()) {
-      if (Dst.mayReadFromMemory())
-        info.hazards |= DependenceHazard::Flow;
-
-      if (Dst.mayWriteToMemory())
-        info.hazards |= DependenceHazard::Out;
-    }
-
-    addDependenceWithInfo(*src, Dst, info);
-  }
-
   void visitMemRefInstruction(llvm::Instruction &CurInstruction) {
     auto query = MDA.getDependency(&CurInstruction);
 
@@ -177,11 +75,82 @@ private:
     }
   }
 
+  decltype(auto) determineHazard(const llvm::Instruction &Src,
+                                 const llvm::Instruction &Dst,
+                                 const llvm::MemDepResult &QueryResult) {
+    BasicDependenceInfo info{DependenceOrigin::Memory,
+                             DependenceHazard::Unknown};
+
+    // TODO decide what to do when the query is unknown
+    if (QueryResult.isUnknown())
+      return info;
+
+    if (QueryResult.isDef()) {
+      if (Src.mayReadFromMemory() && Dst.mayReadFromMemory())
+        // TODO decide how to handle these cases (if any) better
+        assert(true && "A RAW hazard was reported!");
+
+      if (Src.mayReadFromMemory() && Dst.mayWriteToMemory())
+        info.hazards |= DependenceHazard::Anti;
+
+      if (Src.mayWriteToMemory() && Dst.mayReadFromMemory())
+        info.hazards |= DependenceHazard::Flow;
+
+      if (Src.mayWriteToMemory() && Dst.mayWriteToMemory())
+        info.hazards |= DependenceHazard::Out;
+    } else if (QueryResult.isClobber()) {
+      if (Dst.mayReadFromMemory())
+        info.hazards |= DependenceHazard::Flow;
+
+      if (Dst.mayWriteToMemory())
+        info.hazards |= DependenceHazard::Out;
+    }
+
+    return info;
+  }
+
   void addDependenceWithInfo(llvm::Instruction &Src, llvm::Instruction &Dst,
                              const BasicDependenceInfo &Info) {
     auto src = Graph.getOrInsertNode(&Src);
     auto dst = Graph.getOrInsertNode(&Dst);
     src->addDependentNode(dst, Info);
+  }
+
+  void getInterproceduralDependees(llvm::CallSite Dst) {
+    if (!Dst)
+      return;
+
+    auto &results = MDA.getNonLocalCallDependency(Dst);
+    auto dst = Dst.getInstruction();
+
+    for (auto &e : results) {
+      auto &result = e.getResult();
+      auto src = result.getInst();
+      auto info = determineHazard(*src, *dst, result);
+
+      addDependenceWithInfo(*src, *dst, info);
+    }
+  }
+
+  void getFunctionLocalDependees(llvm::Instruction &Dst) {
+    llvm::SmallVector<llvm::NonLocalDepResult, 8> result;
+    MDA.getNonLocalPointerDependency(&Dst, result);
+
+    for (const auto &e : result) {
+      auto &result = e.getResult();
+      auto src = result.getInst();
+      auto info = determineHazard(*src, Dst, result);
+
+      addDependenceWithInfo(*src, Dst, info);
+    }
+  }
+
+  void getBlockLocalDependees(llvm::MemDepResult &Query,
+                              llvm::Instruction &Dst) {
+    auto src = Query.getInst();
+    auto info = determineHazard(*src, Dst, Query);
+
+    addDependenceWithInfo(*src, Dst, info);
   }
 };
 
