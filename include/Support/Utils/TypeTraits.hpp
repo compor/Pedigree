ded9d20dc711c39e8188ebffdff22ab4068466e3
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-04-12 23:58:04 +0100

    add utility trait class

--- include/Support/Utils/TypeTraits.hpp
+++ include/Support/Utils/TypeTraits.hpp
@@ -6,10 +6,21 @@
 #define PEDIGREE_TYPETRAITS_HPP
 
 #include <type_traits>
+// using std::integral_constant
 // using std::is_nothrow_move_constructible
 // using std::is_nothrow_move_assignable
 
 namespace pedigree {
+
+template <bool... B> struct conjuction {};
+
+template <bool B, bool... Rest>
+struct conjuction<B, Rest...>
+    : std::integral_constant<bool, B && conjuction<Rest...>::value> {};
+
+template <bool B> struct conjuction<B> : std::integral_constant<bool, B> {};
+
+//
 
 template <typename T, typename... TArgs>
 struct are_all_nothrow_move_constructible {
