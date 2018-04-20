c9d80602d405caec916f6253ad749c6751e807aa
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-04-20 10:20:37 +0100

    add namespace scope

--- include/Analysis/AnalysisScope.hpp
+++ include/Analysis/AnalysisScope.hpp
@@ -6,11 +6,11 @@
 #define PEDIGREE_ANALYSISSCOPE_HPP
 
 #include <cstdint>
-// using uint8_t
+// using std::uint8_t
 
 namespace pedigree {
 
-enum class AnalysisScope : uint8_t {
+enum class AnalysisScope : std::uint8_t {
   // Unknown
   Block,
   Function,
