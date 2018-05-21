2604f0232528b479793a12acfe6a64f7a0f2a49a
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-05-21 13:07:40 +0100

    add graph operation

---
+++ include/Analysis/Operations/DFSEnumerate.hpp
@@ -0,0 +1,45 @@
+//
+//
+//
+
+#ifndef PEDIGREE_DFSENUMERATE_HPP
+#define PEDIGREE_DFSENUMERATE_HPP
+
+#include "Config.hpp"
+
+#include "llvm/IR/BasicBlock.h"
+// using llvm::BasicBlock
+
+#include "llvm/IR/CFG.h"
+
+#include "llvm/ADT/DepthFirstIterator.h"
+// using llvm::depth_first
+
+#include <memory>
+// using std::addressof
+
+#include <iterator>
+// using std::iterator_traits
+
+namespace pedigree {
+
+template <typename GraphT, typename UnitT>
+void DFSEnumerate(GraphT &G, const UnitT &Unit) {
+  auto dfs = llvm::depth_first(std::addressof(const_cast<UnitT &>(Unit)));
+  unsigned int i = 0;
+
+  for (auto *e : dfs) {
+    for (auto &k : *e) {
+      auto node = G.getNode(std::addressof(k));
+
+      if (node) {
+        auto &info = (*node)->info();
+        info.id = i++;
+      }
+    }
+  }
+}
+
+} // namespace pedigree
+
+#endif // header
