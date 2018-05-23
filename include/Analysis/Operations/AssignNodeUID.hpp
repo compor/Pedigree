3ad81eba3aa3e59b97b96bb70666fbe3b15f4148
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-05-23 10:14:15 +0100

    change DFS numbering order and add static assertions

--- include/Analysis/Operations/AssignNodeUID.hpp
+++ include/Analysis/Operations/AssignNodeUID.hpp
@@ -2,46 +2,48 @@
 //
 //
 
-#ifndef PEDIGREE_DFSENUMERATE_HPP
-#define PEDIGREE_DFSENUMERATE_HPP
+#ifndef PEDIGREE_ASSIGNNODEUID_HPP
+#define PEDIGREE_ASSIGNNODEUID_HPP
 
 #include "Config.hpp"
-
-#include "llvm/IR/BasicBlock.h"
-// using llvm::BasicBlock
-
-#include "llvm/IR/CFG.h"
-
-#include <memory>
-// using std::addressof
 
 #include <iterator>
 // using std::iterator_traits
 
+#include <algorithm>
+// using std::for_each
+
 #include <type_traits>
 // using std::is_pointer
-
-#include <utility>
-// using std::declval
+// using std::is_same
 
 namespace pedigree {
 
-template <typename GraphT, typename IteratorT>
-void AssignNodeUID(GraphT &G, IteratorT Begin, IteratorT End) {
-  unsigned int i = 0;
+namespace detail {
 
+template <typename GraphT>
+using NodeInfoType = typename GraphT::NodeType::NodeInfoType::value_type;
+
+} // namespace detail
+
+template <typename GraphT, typename IteratorT>
+void AssignNodeUID(GraphT &G, IteratorT Begin, IteratorT End,
+                   typename detail::NodeInfoType<GraphT>::IDTy FirstID = 0) {
   static_assert(!std::is_pointer<GraphT>::value,
                 "Graph type cannot be a pointer!");
+  static_assert(
+      std::is_same<typename GraphT::NodeType::UnitType,
+                   typename std::iterator_traits<IteratorT>::value_type>::value,
+      "Graph node unit does not match iteration unit!");
 
   std::for_each(Begin, End, [&](const auto &e) {
+    ++FirstID;
     auto node = G.getNode(e);
 
     if (node) {
       auto &info = (*node)->info();
-      info.id = i;
+      info.id = FirstID;
     }
-
-    ++i;
   });
 }
 
