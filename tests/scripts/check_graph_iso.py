f17a0a5e383ab14d8bcff0b9c96f20e89914f551
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-05-29 16:24:49 +0100

    add doc

--- tests/scripts/check_graph_iso.py
+++ tests/scripts/check_graph_iso.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python
-""" A command line utility that checks if two digraphs in GraphViz DOT format
+"""A command line utility that checks if two digraphs in GraphViz DOT format
 are isomorphic.
 
 The utility accepts the names of the two files containing GraphViz DOT digraphs
-and outputs the string "OK" if they are isomorphic.
+and outputs the string "OK" if they are isomorphic. Optionally, it can also
+check for matching values for node attributes between the graphs.
 """
 
 from __future__ import print_function
 
-import sys
-import os
+import networkx as nx
 
 from argparse import ArgumentParser
-
-import networkx as nx
 
 from networkx.drawing.nx_agraph import read_dot
 
 
 def find_nodes_with_attributes(g, **attr):
+    """Find all the nodes in a graph with the specified attributes and their
+    corresponding values.
+
+    Args:
+        g (graph): Graph object.
+        attr: Arbitrary keyword arguments to use for finding matching graph
+        nodes (same attribute names and corresponding values).
+
+    Returns:
+        list: A list of all the nodes found with matching attributes and
+        their corresponding values.
+    """
+
     found = []
     nodes = g.nodes(data=True)
 
@@ -31,6 +42,19 @@ def find_nodes_with_attributes(g, **attr):
 
 
 def are_isomorphic_with_attribute(g1, g2, node_attribute):
+    """Checks if 2 graphs are isomorphic by matching nodes with specific
+    attributes and their corresponding values.
+
+    Args:
+        g1 (graph): Graph object.
+        g2 (graph): Graph object.
+        node_attribute (string): Name of the graph node attribute that is used
+        to check for isomorphism.
+
+    Returns:
+        bool: True when graphs are isomorphic, false otherwise.
+    """
+
     are_iso = True
 
     while are_iso and len(g1):
@@ -50,7 +74,7 @@ def are_isomorphic_with_attribute(g1, g2, node_attribute):
 
 
 def check_graph_iso(dotfile1, dotfile2, node_attr=[]):
-    """ Checks if two GraphViz DOT digraphs are isomorphic.
+    """Checks if two GraphViz DOT digraphs are isomorphic.
 
     Args:
         dotfile1 (string): Input file name expected to contain a GraphViz DOT
