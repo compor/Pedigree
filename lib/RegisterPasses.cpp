16688acec17a455318b45579d0bf1c0979e16a37
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-02-21 20:12:48 +0000

    Provide plugin registration for new passmanager

---
+++ lib/RegisterPasses.cpp
@@ -0,0 +1,70 @@
+//
+//
+//
+
+#include "Pedigree/Config.hpp"
+
+#include "Pedigree/Util.hpp"
+
+#include "Pedigree/Analysis/Passes/DDGraphPass.hpp"
+
+#include "llvm/IR/PassManager.h"
+// using llvm::FunctionAnalysisManager
+
+#include "llvm/Passes/PassBuilder.h"
+// using llvm::PassBuilder
+
+#include "llvm/Passes/PassPlugin.h"
+// using llvmGetPassPluginInfo
+
+#include "llvm/ADT/StringRef.h"
+// using llvm::StringRef
+
+#include "llvm/Support/Debug.h"
+// using LLVM_DEBUG macro
+// using llvm::dbgs
+
+#define DEBUG_TYPE "pedigree-plugin-registration"
+
+// plugin registration for opt new passmanager
+
+namespace {
+
+void registerCallbacks(llvm::PassBuilder &PB) {
+  PB.registerAnalysisRegistrationCallback(
+      [](llvm::FunctionAnalysisManager &FAM) {
+        LLVM_DEBUG(llvm::dbgs() << "registering analysis "
+                                << PEDIGREE_DDG_PASS_NAME << "\n";);
+        FAM.registerPass([]() { return pedigree::DDGraphPass(); });
+      });
+  PB.registerPipelineParsingCallback(
+      [](llvm::StringRef Name, llvm::FunctionPassManager &FPM,
+         llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
+        if (Name == "require<" PEDIGREE_DDG_PASS_NAME ">") {
+          LLVM_DEBUG(llvm::dbgs() << "registering require analysis parser for "
+                                  << PEDIGREE_DDG_PASS_NAME << "\n";);
+
+          FPM.addPass(llvm::RequireAnalysisPass<pedigree::DDGraphPass,
+                                                llvm::Function>());
+          return true;
+        }
+        if (Name == "invalidate<" PEDIGREE_DDG_PASS_NAME ">") {
+          LLVM_DEBUG(llvm::dbgs()
+                         << "registering invalidate analysis parser for "
+                         << PEDIGREE_DDG_PASS_NAME << "\n";);
+
+          FPM.addPass(llvm::InvalidateAnalysisPass<pedigree::DDGraphPass>());
+          return true;
+        }
+        return false;
+      });
+}
+
+} // namespace
+
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
+llvmGetPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "PedigreePlugin", STRINGIFY(VERSION_STRING),
+          registerCallbacks};
+}
+
