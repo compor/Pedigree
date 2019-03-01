c40077f6bb0ce0f387447d4d2c2ee0de6cc169fc
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-03-01 18:58:46 +0000

    Use cache to skip undesired instructions

--- include/Pedigree/Analysis/Creational/DDGraphBuilder.hpp
+++ include/Pedigree/Analysis/Creational/DDGraphBuilder.hpp
@@ -17,6 +17,9 @@
 #include "llvm/ADT/Optional.h"
 // using llvm::Optional
 
+#include "llvm/ADT/SmallPtrSet.h"
+// using llvm::SmallPtrSet
+
 #include "llvm/Support/Debug.h"
 // using LLVM_DEBUG macro
 // using llvm::dbgs
@@ -37,6 +40,47 @@ class DDGraphBuilder : public llvm::InstVisitor<DDGraphBuilder> {
   DDGraphResultT Graph;
   llvm::Optional<const llvm::Function *> CurUnit;
   bool shouldIgnoreConstantPHINodes = false;
+
+  llvm::SmallPtrSet<llvm::Instruction *, 32> SkipSet;
+
+  bool shouldSkip(llvm::IntrinsicInst &CurI) {
+    auto intrID = CurI.getIntrinsicID();
+
+    switch (intrID) {
+    default:
+    case llvm::Intrinsic::lifetime_start:
+    case llvm::Intrinsic::lifetime_end:
+    case llvm::Intrinsic::invariant_start:
+    case llvm::Intrinsic::invariant_end:
+    case llvm::Intrinsic::launder_invariant_group:
+    case llvm::Intrinsic::strip_invariant_group:
+      return true;
+    }
+
+    return false;
+  }
+
+  bool shouldSkip(llvm::Instruction *CurI) {
+    assert(CurI && "Pointer is null!");
+
+    if (SkipSet.count(CurI)) {
+      return true;
+    }
+
+    bool status = false;
+
+    if (auto *ii = llvm::dyn_cast<llvm::IntrinsicInst>(CurI)) {
+      status |= shouldSkip(*ii);
+    }
+
+    if (status) {
+      SkipSet.insert(CurI);
+    }
+
+    return status;
+  }
+
+  bool shouldSkip(llvm::Instruction &CurI) { return shouldSkip(&CurI); }
 
 public:
   DDGraphBuilder() = default;
@@ -65,9 +109,13 @@ public:
   }
 
   void visitInstruction(llvm::Instruction &CurI) {
+    if (shouldSkip(CurI)) {
+      return;
+    }
+
     for (auto &u : CurI.uses()) {
       auto *user = llvm::dyn_cast<llvm::Instruction>(u.getUser());
-      if (user) {
+      if (user && !shouldSkip(user)) {
         auto src = Graph->getOrInsertNode(&CurI);
         auto dst = Graph->getOrInsertNode(user);
         src->addDependentNode(dst, {DO_Data, DH_Flow});
@@ -76,6 +124,10 @@ public:
   }
 
   void visitPHINode(llvm::PHINode &CurI) {
+    if (shouldSkip(CurI)) {
+      return;
+    }
+
     if (!shouldIgnoreConstantPHINodes) {
       for (auto &e : CurI.incoming_values()) {
         if (llvm::isa<llvm::Constant>(e.get())) {
@@ -88,29 +140,6 @@ public:
     }
 
     visitInstruction(CurI);
-  }
-
-  void visitIntrinsicInst(llvm::IntrinsicInst &CurI) {
-    auto intrID = CurI.getIntrinsicID();
-    bool hasChanged = false;
-
-    switch (intrID) {
-    default:
-      visitInstruction(CurI);
-    case llvm::Intrinsic::lifetime_start:
-    case llvm::Intrinsic::lifetime_end:
-    case llvm::Intrinsic::invariant_start:
-    case llvm::Intrinsic::invariant_end:
-    case llvm::Intrinsic::launder_invariant_group:
-    case llvm::Intrinsic::strip_invariant_group:
-      hasChanged = Graph->removeNode(&CurI);
-
-      LLVM_DEBUG(if (hasChanged) {
-        llvm::dbgs() << "remove intrinsic already in graph\n";
-      });
-
-      break;
-    }
   }
 };
 
